a9ed66b4bc83ddeb6831cfc7cf16d8269dfeca22
ryo63 <ihara.ryo.63@gmail.com>
2023-02-15 13:09:01 +0900

    2023.02.14.studied

---
+++ src/06section/myfiestclass.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+john = Person("John", 28, "male")
+taro = Person("Taro", 18, "male")
+emma = Person("Emma", 20, "female")
